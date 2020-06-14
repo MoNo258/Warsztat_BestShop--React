f6545501f97580aae6a33f49a6096a4917a10250
Monika N <monika258@gmail.com>
2020-06-14 22:58:39 +0200

    section stats in progress

--- src/js/SectionStats.js
+++ src/js/SectionStats.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import StatsArticle from "./StatsArticle";
 
 class SectionStats extends Component{
     render() {
@@ -9,41 +10,7 @@ class SectionStats extends Component{
                         The most popular sales platform in country
                     </h2>
                     <div className="stats-list">
-
-                        <article className="stats-card">
-                            <div className="stats-card_pseudo stats-card_pseudo-first">
-                                <img src={"../images/person.svg"} alt="" className="stats-card_img" />
-                            </div>
-                            <h2 className="stats-card__title stats-card__title-first">
-                                100000+
-                            </h2>
-                            <p className="stats-card__text">
-                                daily entries
-                            </p>
-                        </article>
-                        <article className="stats-card">
-                            <div className="stats-card_pseudo stats-card_pseudo-second">
-                                <img src={"../images/eye.svg"} alt="" className="stats-card_img stats-card_img-eye" />
-                            </div>
-                            <h2 className="stats-card__title stats-card__title-second">
-                                2000000+
-                            </h2>
-                            <p className="stats-card__text">
-                                seen products every day
-                            </p>
-                        </article>
-                        <article className="stats-card">
-                            <div className="stats-card_pseudo stats-card_pseudo-third">
-                                <img src={"../images/catalog.svg"} alt="" className="stats-card_img" />
-                            </div>
-                            <h2 className="stats-card__title stats-card__title-third">
-                                10000+
-                            </h2>
-                            <p className="stats-card__text">
-                                added posts daily
-                            </p>
-                        </article>
-
+                        <StatsArticle />
                     </div>
                 </div>
             </section>
---
+++ src/js/StatsArticle.js
@@ -0,0 +1,58 @@
+import React, {Component} from "react";
+
+const articleParams = [
+    {
+        divClass: 'stats-card_pseudo stats-card_pseudo-first',
+        imgSrc: '../images/person.svg',
+        imgClass: 'stats-card_img',
+        headerClass: 'stats-card__title stats-card__title-first',
+        headerText: '100000+',
+        paragraphClass: 'stats-card__text',
+        paragraphText: 'daily entries'
+    },
+    {
+        divClass: 'stats-card_pseudo stats-card_pseudo-second',
+        imgSrc: '../images/eye.svg',
+        imgClass: 'stats-card_img stats-card_img-eye',
+        headerClass: 'stats-card__title stats-card__title-second',
+        headerText: '2000000+',
+        paragraphClass: 'stats-card__text',
+        paragraphText: 'seen products every day'
+    },
+    {
+        divClass: 'stats-card_pseudo stats-card_pseudo-third',
+        imgSrc: '../images/catalog.svg',
+        imgClass: 'stats-card_img',
+        headerClass: 'stats-card__title stats-card__title-third',
+        headerText: '10000+',
+        paragraphClass: 'stats-card__text',
+        paragraphText: 'added posts daily'
+    }
+];
+
+class StatsArticle extends Component {
+    render() {
+        return (
+            <article className="stats-card">
+                {articleParams.map(param => {
+                    return (
+                        <>
+                            <div className={param.divClass}>
+                                <img src={param.imgSrc} alt="" className={param.imgClass}/>
+                            </div>
+                            <h2 className={param.headerClass}>
+                                {param.headerText}
+                            </h2>
+                            <p className={param.paragraphClass}>
+                                {param.paragraphText}
+                            </p>
+                        </>
+                    )
+                })}
+            </article>
+        )
+    }
+}
+
+
+export default StatsArticle;
